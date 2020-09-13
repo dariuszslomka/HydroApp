c90ca2eae4e4d6591e82a4439480ddcdb7e61413
dfrugo <dariusz.slomka88@gmail.com>
2020-09-13 19:53:01 +0200

    implement changing glass counter

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,45 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
-/* place your code below */
+let counter = getGlassCounter();
+if(counter)
+{
+    refreshGlassNumber(counter);
+}
 
-console.log('HELLO 🚀')
+let addButton = document.querySelector(".add-button--js");
+addButton.addEventListener("click", () => {
+  let counter = getGlassCounter();
+  setGlassCounter(++counter);
+});
+
+let removeButton = document.querySelector(".remove-button--js");
+removeButton.addEventListener("click", () => {
+  let counter = getGlassCounter();
+  if (counter == 0) {
+    return;
+  }
+  setGlassCounter(--counter);
+});
+
+function refreshGlassNumber(glassNumber) {
+  let counterText = document.querySelector(".glass__number--js");
+  counterText.innerHTML = glassNumber;
+}
+
+function getGlassCounter() {
+  let glassCounter = localStorage.getItem("glassCounter");
+  if (glassCounter) {
+    return parseInt(glassCounter);
+  } else {
+    return 0;
+  }
+}
+
+function setGlassCounter(glassNumber) {
+  localStorage.setItem("glassCounter", glassNumber);
+  refreshGlassNumber(glassNumber);
+}
